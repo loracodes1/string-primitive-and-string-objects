ac1292c623c8cbb51ff36064751935cb181fb425
florence <loracodes3@gmail.com>
2024-09-27 15:13:46 +0300

    literal is string primitive

---
+++ index.js
@@ -0,0 +1,5 @@
+const strPrim = "foo"; // A literal is a string primitive
+const strPrim2 = String(1); // Coerced into the string primitive "1"
+const strPrim3 = String(true); // Coerced into the string primitive "true"
+const strObj = new String(strPrim); // String with new returns a string wrapper object.
+
