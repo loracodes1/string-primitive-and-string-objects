a04bb832a70cb63de703dce19614e303b8ae9fad
florence <loracodes3@gmail.com>
2024-09-27 15:18:50 +0300

    typeOf

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const strPrim2 = String(1); // Coerced into the string primitive "1"
 const strPrim3 = String(true); // Coerced into the string primitive "true"
 const strObj = new String(strPrim); // String with new returns a string wrapper object.
 
+console.log(typeof strPrim); // "string"
+console.log(typeof strPrim2); // "string"
+console.log(typeof strPrim3); // "string"
+console.log(typeof strObj); // "object"
+
