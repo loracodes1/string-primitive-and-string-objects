1f2db01a7b582cf54386207f1a02b2e614fc1bac
florence <loracodes3@gmail.com>
2024-09-27 15:15:09 +0300

    coerce into the first string primitive

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 const strPrim = "foo"; // A literal is a string primitive
 const strPrim2 = String(1); // Coerced into the string primitive "1"
-const strPrim3 = String(true); // Coerced into the string primitive "true"
-const strObj = new String(strPrim); // String with new returns a string wrapper object.
 
