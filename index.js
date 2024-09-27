c61bc4eedf30f6d1e0bca8975ea90649517dfaa2
florence <loracodes3@gmail.com>
2024-09-27 15:16:47 +0300

    coerce into string primitive

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const strPrim = "foo"; // A literal is a string primitive
 const strPrim2 = String(1); // Coerced into the string primitive "1"
+const strPrim3 = String(true); // Coerced into the string primitive "true"
 
